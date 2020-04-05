39f641f036910fd1b55b37305628ca4acb2d523a
kyubuns <kyubuns@gmail.com>
2020-04-05 22:23:21 +0900

    feat: Text表示

---
+++ Unity/Assets/Scripts/Test.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private Text logText = default;
+
+    void Start()
+    {
+        logText.text = "Init...";
+    }
+}
