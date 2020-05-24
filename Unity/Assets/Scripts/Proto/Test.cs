87121363c343cef1b6cc5605a3c4ba81d653407a
kyubuns <kyubuns@gmail.com>
2020-05-24 15:05:47 +0900

    feat: Resourcesからtext読むまで

---
+++ Unity/Assets/Scripts/Proto/Test.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        var luaText = Resources.Load<TextAsset>("lua");
+        Debug.Log(luaText);
+    }
+}
