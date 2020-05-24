298bb6349f2c89862e9e130ef679e07180edaf26
kyubuns <kyubuns@gmail.com>
2020-05-24 18:17:53 +0900

    feat: FileServer

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,27 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
 using Fleck;
 
 namespace Server
 {
     public static class Program
     {
-        public static void Main()
+        public static async Task Main(string[] args)
         {
-            const int port = 5555;
-            var url = $"ws://{GetLocalIp()}:{port}";
+            var fileWatcher = new FileWatcher(Path.GetFullPath(args[0]));
+            var webServer = new WebServer(5555, fileWatcher);
+            webServer.Start();
+            Console.WriteLine($"Start Server {webServer.Url}");
+
+            while (true)
+            {
+                await fileWatcher.WaitForUpdate();
+                Console.WriteLine("Update");
+                webServer.Broadcast();
+            }
+        }
+    }
+
+    public class WebServer
+    {
+        private readonly int port;
+        private readonly FileWatcher fileWatcher;
+        private readonly List<IWebSocketConnection> sockets = new List<IWebSocketConnection>();
+
+        public string Url => $"ws://{GetLocalIp()}:{port}";
+
+        public WebServer(int port, FileWatcher fileWatcher)
+        {
+            this.port = port;
+            this.fileWatcher = fileWatcher;
+        }
+
+        public void Start()
+        {
             var webSocketServer = new WebSocketServer($"ws://0.0.0.0:{port}");
 
             webSocketServer.Start(socket =>
             {
-                socket.OnOpen += () => { Console.WriteLine("OnOpen"); };
-                socket.OnClose += () => { Console.WriteLine("OnClose"); };
-                socket.OnError += e => { Console.WriteLine($"OnError {e}"); };
+                socket.OnOpen += () =>
+                {
+                    Console.WriteLine("OnOpen");
+                    socket.Send(fileWatcher.Current);
+                    sockets.Add(socket);
+                };
+                socket.OnClose += () =>
+                {
+                    Console.WriteLine("OnClose");
+                    sockets.Remove(socket);
+                };
+                socket.OnError += e =>
+                {
+                    Console.WriteLine($"OnError {e}");
+                    sockets.Remove(socket);
+                };
             });
+        }
 
-            Console.WriteLine($"Start {url}");
-            Console.ReadLine();
+        public void Broadcast()
+        {
+            foreach (var socket in sockets)
+            {
+                socket.Send(fileWatcher.Current);
+            }
         }
 
         // https://stackoverflow.com/questions/6803073/get-local-ip-address
@@ -34,6 +84,60 @@ namespace Server
                 if (socket.LocalEndPoint is IPEndPoint endPoint) localIp = endPoint.Address.ToString();
             }
             return localIp;
+        }
+    }
+
+    public class FileWatcher
+    {
+        private readonly string filePath;
+
+        public string Current { get; private set; }
+
+        public FileWatcher(string filePath)
+        {
+            this.filePath = filePath;
+            Current = File.ReadAllText(filePath, Encoding.UTF8);
+        }
+
+        public Task WaitForUpdate()
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+            var watcher = new FileSystemWatcher();
+            watcher.IncludeSubdirectories = true;
+            Console.WriteLine($"Path.GetDirectoryName(filePath) = {Path.GetDirectoryName(filePath)}");
+            Console.WriteLine($"Path.GetFileName(filePath) = {Path.GetFileName(filePath)}");
+            watcher.Path = Path.GetDirectoryName(filePath);
+            watcher.Filter = Path.GetFileName(filePath);
+            watcher.NotifyFilter = (NotifyFilters.LastAccess
+                                    | NotifyFilters.LastWrite
+                                    | NotifyFilters.FileName
+                                    | NotifyFilters.DirectoryName
+                                    | NotifyFilters.CreationTime
+                                    | NotifyFilters.Size);
+
+            void OnUpdate(object sender, FileSystemEventArgs e)
+            {
+                Console.WriteLine($"OnUpdate {sender}, {e.Name} {e.ChangeType} {e.FullPath}");
+                try
+                {
+                    Current = File.ReadAllText(e.FullPath, Encoding.UTF8);
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine("File not found");
+                    return;
+                }
+                watcher.Dispose();
+                taskCompletionSource.SetResult(true);
+            }
+
+            watcher.Created += OnUpdate;
+            watcher.Changed += OnUpdate;
+            watcher.Deleted += OnUpdate;
+            watcher.Renamed += OnUpdate;
+
+            watcher.EnableRaisingEvents = true;
+            return taskCompletionSource.Task;
         }
     }
 }
