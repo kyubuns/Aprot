88164b9a751bff04469f9ed5a4c7c396114c900c
kyubuns <kyubuns@gmail.com>
2020-05-24 16:55:41 +0900

    feat: WebSocketSharpを使って通信まで実機で確認

--- Unity/Assets/Aprot/Scripts/Engine.cs
+++ Unity/Assets/Aprot/Scripts/Engine.cs
@@ -1,6 +1,22 @@
-﻿namespace Aprot
+﻿using UnityEngine;
+using WebSocketSharp;
+
+namespace Aprot
 {
     public class Engine
     {
+        public static void ConnectDevelopmentServer(string url)
+        {
+            var webSocket = new WebSocket(url);
+            webSocket.Log.Level = LogLevel.Trace;
+
+            webSocket.OnOpen += (sender, e) => { Debug.Log($"OnOpen {sender}, {e}"); };
+            webSocket.OnClose += (sender, e) => { Debug.Log($"OnClose {sender}, {e.Reason}"); };
+            webSocket.OnMessage += (sender, e) => { Debug.Log($"OnMessage {sender} {e}"); };
+            webSocket.OnError += (sender, e) => { Debug.LogError($"OnError {sender} {e}"); };
+
+            Debug.Log("Connecting...");
+            webSocket.Connect();
+        }
     }
 }
--- Unity/Assets/Scripts/ProtoTest.cs
+++ Unity/Assets/Scripts/ProtoTest.cs
@@ -1,8 +1,25 @@
-﻿using UnityEngine;
+﻿using Aprot;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Proto
 {
     public class ProtoTest : MonoBehaviour
     {
+        [SerializeField] private Text debugText = default;
+        [SerializeField] private InputField inputField = default;
+        [SerializeField] private Button connectButton = default;
+
+        public void Start()
+        {
+            inputField.text = PlayerPrefs.GetString("DebugKey", "");
+            connectButton.onClick.AddListener(() =>
+            {
+                Debug.Log($"Connect to {inputField.text}");
+                PlayerPrefs.SetString("DebugKey", inputField.text);
+
+                Engine.ConnectDevelopmentServer(inputField.text);
+            });
+        }
     }
 }
