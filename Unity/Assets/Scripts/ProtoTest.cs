59fc436110c47d75ddd22d471d1ea6f93afc37f6
kyubuns <kyubuns@gmail.com>
2020-05-31 18:00:15 +0900

    feat: 今度はフリーズするようになった

--- Unity/Assets/Scripts/ProtoTest.cs
+++ Unity/Assets/Scripts/ProtoTest.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Aprot;
 using proto.inputContext;
 using UnityEngine;
 using UnityEngine.UI;
+using Time = UnityEngine.Time;
 using Vector2 = aprotHx.type.Vector2;
 
 namespace Proto
@@ -12,11 +14,15 @@ namespace Proto
         [SerializeField] private Text debugText = default;
         [SerializeField] private InputField inputField = default;
         [SerializeField] private Button connectButton = default;
+        [SerializeField] private GameObject box = default;
+        private List<GameObject> boxCache = new List<GameObject>();
 
         private Engine engine;
+        private readonly FpsCounter fpsCounter = new FpsCounter();
 
         public void Start()
         {
+            Application.targetFrameRate = 60;
             inputField.text = PlayerPrefs.GetString("DebugKey", "");
             connectButton.onClick.AddListener(() =>
             {
@@ -26,25 +32,66 @@ namespace Proto
                 engine = new Engine();
                 engine.ConnectDevelopmentServer(inputField.text);
             });
+
+            box.SetActive(false);
         }
 
         public void Update()
         {
+            UpdateEngine();
+            UpdateFpsCounter();
+        }
+
+        private void UpdateEngine()
+        {
             if (engine == null) return;
 
-            var time = new proto.inputContext.Time(UnityEngine.Time.deltaTime);
+            var time = new proto.inputContext.Time(Time.deltaTime);
             var input = new proto.inputContext.Input(new Vector2(0, 0));
             var inputContext = new InputContext(time, input);
             var outputContext = engine.Update(inputContext);
             if (outputContext?.renderer?.queue == null) return;
 
             var renderQueue = outputContext.renderer.queue.toTyped();
-            Debug.Log($"{renderQueue[0].x}, {renderQueue[0].y}");
+            foreach (var (x, i) in renderQueue.Select((x, i) => (x, i)))
+            {
+                Debug.Log($"{x.x}, {x.y}");
+                if (boxCache.Count <= i)
+                {
+                    var newBox = GameObject.Instantiate(box);
+                    newBox.SetActive(true);
+                    boxCache.Add(newBox);
+                }
+
+                boxCache[i].transform.localPosition = new Vector3((float) x.x, (float) x.y, 0f);
+            }
+        }
+
+        private void UpdateFpsCounter()
+        {
+            fpsCounter.Update();
+            debugText.text = $"{fpsCounter.Fps:0.0}FPS";
         }
 
         public void OnDestroy()
         {
             engine?.Dispose();
+        }
+    }
+
+    // https://techblog.kayac.com/approximate-average-fps
+    public class FpsCounter
+    {
+        public float Fps => 1f / avgTime;
+
+        private const float K = 0.05f;
+        private float avgTime;
+
+        public void Update()
+        {
+            var dt = Time.deltaTime;
+            avgTime *= 1f - K;
+            avgTime += dt * K;
         }
     }
 }
