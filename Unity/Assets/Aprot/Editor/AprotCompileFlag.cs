9a2f29052481fb99bb0144d32a61374856ecf969
kyubuns <kyubuns@gmail.com>
2020-06-06 13:43:14 +0900

    feat: compile menu

---
+++ Unity/Assets/Aprot/Editor/AprotCompileFlag.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Aprot.Editor
+{
+    public static class AprotCompileFlag
+    {
+        [MenuItem("Aprot/Full C#")]
+        public static void ChangeToFullCsharp()
+        {
+            Debug.Log("ChangeToFullCsharp");
+        }
+    }
+}
